d4450bf8f188df24f1292e37bcd82955917ea885
arsgooo <106697811+arsgooo@users.noreply.github.com>
2024-02-12 13:03:26 +0200

    Lab 1 done

--- Lab1/task1/task1.py
+++ Lab1/task1/task1.py
@@ -7,11 +7,22 @@
 
 class GeometricProgression:
     __counter = 0 # static variable to count the instances
+    __instances = {}
+
+    @staticmethod
+    def print_instances():
+        print("\nInformation about all instances:")
+        for id, inst in GeometricProgression.__instances.items():
+            print(f"#{id} {inst}")
+        return f"Existing instances: {len(GeometricProgression.__instances)}"
     
     def __init__(self, b, q):
-        self.b = b  # first term
+        self.b = b  # first term of geometric progression
         self.q = q  # common ratio
+        self.id = str(id(self))
+        GeometricProgression.__instances[self.id] = self
         GeometricProgression.__counter += 1
+        
 
     def first_element(self):
         return self.b
@@ -30,22 +41,18 @@ class GeometricProgression:
         sequence = self.sequence_from_k_to_m(1, 7)
         return f"& {self.b}, {self.q}: {{{', '.join(map(str, sequence))} ...}}"
 
-    @staticmethod
-    def instances_count():
-        return f"There are {GeometricProgression.__counter} instances now."
+gp1 = GeometricProgression(1, 4)
+print("PROGRESSION 1")
+print(f"First element of progression1 is {gp1.first_element()}")
+print(gp1)  # & 1, 4: {1, 4, 16, 64, 256, 1024, 4096 ...}
 
-progression1 = GeometricProgression(1, 4)
-print("PROGRESSION1")
-print(f"First element of progression1 is {progression1.first_element()}")
-print(progression1)  # & 1, 4: {1, 4, 16, 64, 256, 1024, 4096 ...}
+gp2 = GeometricProgression(3, 3)
+print("\nPROGRESSION 2")
+print(f"Fifth element of progression2 is {gp2.n_element(5)}")
+print(gp2)  # & 3, 3: {3, 9, 27, 81, 243, 729, 2187 ...}
 
-progression2 = GeometricProgression(3, 3)
-print("\nPROGRESSION2")
-print(f"Fifth element of progression2 is {progression2.n_element(5)}")
-print(progression2)  # & 3, 3: {3, 9, 27, 81, 243, 729, 2187 ...}
+gp2.change_parameters(7, 2)
+print("\nUPDATED PROGRESSION 2")
+print(gp2)  # & 7, 2: {7, 14, 28, 56, 112, 224, 448 ...}
 
-progression2.change_parameters(7, 2)
-print("\nUPDATED PROGRESSION2")
-print(progression2)  # & 7, 2: {7, 14, 28, 56, 112, 224, 448 ...}
-
-print("\n" + GeometricProgression.instances_count())
+print("\n" + GeometricProgression.print_instances())
--- Lab1/task2/src/GeometricProgression.java
+++ Lab1/task2/src/GeometricProgression.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class GeometricProgression {
+import java.util.ArrayList;
+import java.util.List;
+
+public class GeometricProgression {
+    private static List<GeometricProgression> instances = new ArrayList<>();
+    private static int counter = 0; // static variable to count the instances
+    private int b; // first term of geometric progression
+    private int q; // common ratio
+
+    public static String printAllInstances() {
+        System.out.println("\nInformation about all instances:");
+        for (GeometricProgression instance : instances) {
+            System.out.println(instance);
+        }
+        return "Existing instances: " + counter;
+    }
+
+    public GeometricProgression(int b, int q) {
+        this.b = b;
+        this.q = q;
+        instances.add(this);
+        counter++;
+    }
+
+    public int getB() {
+        return b;
+    }
+
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public int getQ() {
+        return q;
+    }
+
+    public void setQ(int q) {
+        this.q = q;
+    }
+
+    public int firstElement() {
+        return b;
+    }
+
+    public int nElement(int n) {
+        return (int) (b * Math.pow(q, n - 1));
+    }
+
+    public List<Integer> sequenceFromKToM(int k, int m) {
+        List<Integer> sequence = new ArrayList<>();
+        for (int i = k; i <= m; i++) {
+            sequence.add(nElement(i));
+        }
+        return sequence;
+    }
+
+    public void changeParameters(int newB, int newQ) {
+        b = newB;
+        q = newQ;
+    }
+
+    @Override
+    public String toString() {
+        List<Integer> sequence = sequenceFromKToM(1, 7);
+        String elements = String.join(", ", sequence.stream().map(Object::toString).toArray(String[]::new));
+        return String.format("& %d, %d: {%s ...}", b, q, elements);
+    }
 }
